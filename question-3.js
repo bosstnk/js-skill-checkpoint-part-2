755ac417db2f1ff200602b96392c7fdae57bdc3f
thanakorn <thanakorn.stsb@gmail.com>
2026-01-06 17:05:55 +0700

    refactor: update getUsers function to handle errors and improve naming

--- question-3.js
+++ question-3.js
@@ -5,10 +5,14 @@ let getUsers = async () => {
   return data;
 };
 
-getUsers().then((data) => {
-  const result = data
-    .filter((user) => user.name.length > 17)
-    .map((user) => user.name);
+async function getUsersDataNameLengthLower17() {
+  try {
+    const user = await getUsers()
+    const result = user.filter((user) => user.name.length > 17).map((user) => user.name);
+    console.log(result)
+  } catch(error) {
+    console.log("There was an error fetching the data.")
+  };
+}
 
-  console.log(result);
-});
+getUsersDataNameLengthLower17()
